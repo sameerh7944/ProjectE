f52c2cf8203894f3b11199dc6f29c0a2f5947331
sayed <sayed@Sameer.mynetworksettings.com>
2025-01-30 21:28:16 -0500

    new variables added

--- src/Demo1E/Demo1ETest.java
+++ src/Demo1E/Demo1ETest.java
@@ -4,6 +4,21 @@ public class Demo1ETest {
 public static void main(String[]args) {
 		
 		System.out.println("United States Of America");
+		int num = 10;
+        char letter = 'A';
+        boolean isActive = true;
+
+        if (num > 5) {
+            System.out.println("Number is greater than 5");
+        }
+
+        if (letter == 'A') {
+            System.out.println("Letter is A");
+        }
+
+        if (isActive) {
+            System.out.println("The variable isActive is true");
+        }
 }
 
 }
